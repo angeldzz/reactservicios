c450dc4a114e5cd93e2ed9c10620ac832211c635
angeldzz <pintodiazangel@gmail.com>
2025-10-16 14:00:17 +0200

    Servicios apis suppliers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ServiciosApiCustomers from './components/ServiciosApiCustomers';
+import ServiciosApiSuppliers from './components/ServiciosApiSuppliers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ServiciosApiCustomers/>
-  </React.StrictMode>
-);
+    // <ServiciosApiCustomers/>
+    <ServiciosApiSuppliers/>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
