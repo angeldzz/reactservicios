5df82b24fc712d03ba54ca1c5f489dbd74c12018
angeldzz <pintodiazangel@gmail.com>
2025-10-16 14:01:20 +0200

    Servicios apis suppliers

--- src/components/ServiciosApiSuppliers.jsx
+++ src/components/ServiciosApiSuppliers.jsx
@@ -34,6 +34,12 @@ export default class ServiciosApiSuppliers extends Component {
         return (
             <div>
                 <h1>Busqueda con APIS</h1>
+                <input type="text" ref={this.cajaID} />
+                <button onClick={this.buscarSupplier}>Buscar Supplier</button>
+                <h3>{this.state.supplier.SupplierID} - {this.state.supplier.ContactName}</h3>
+                <h4>{this.state.supplier.Region} / {this.state.supplier.City} / {this.state.supplier.Country} / {this.state.supplier.Address}</h4>
+                <h5>{this.state.supplier.PostalCode} / {this.state.supplier.CompanyName} / {this.state.supplier.HomePage}</h5>
+                <h6>{this.state.supplier.ContactTitle} / {this.state.supplier.Fax} / {this.state.supplier.Phone}</h6>
                 {this.state.nombres.map((nombre,index)=>{
                     return(
                         <div>
@@ -41,12 +47,6 @@ export default class ServiciosApiSuppliers extends Component {
                         </div>
                     )
                 })}
-                <input type="text" ref={this.cajaID} />
-                <button onClick={this.buscarSupplier}>Buscar Supplier</button>
-                <h3>{this.state.supplier.SupplierID} - {this.state.supplier.ContactName}</h3>
-                <h4>{this.state.supplier.Region} / {this.state.supplier.City} / {this.state.supplier.Country} / {this.state.supplier.Address}</h4>
-                <h5>{this.state.supplier.PostalCode} / {this.state.supplier.CompanyName} / {this.state.supplier.HomePage}</h5>
-                <h6>{this.state.supplier.ContactTitle} / {this.state.supplier.Fax} / {this.state.supplier.Phone}</h6>
             </div>
         )
     }
